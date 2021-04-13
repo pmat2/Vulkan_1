f97e4f1a79b870fa24fce5fffa7589296261e3a7
Pawel <pawel.matusiak29@gmail.com>
2021-04-13 15:48:00 +0200

    Added pipeline file, printing size of shaders

--- Project1/src/app_one.h
+++ Project1/src/app_one.h
@@ -1,18 +1,20 @@
 #pragma once
 
 #include "lve_window.h"
+#include "lve_pipeline.h"
 
 namespace lve {
 
-class App_one {
-	
-public:
-	static constexpr int WIDTH = 800;
-	static constexpr int HEIGHT = 600;
+	class App_one {
 
-	void run();
+	public:
+		static constexpr int WIDTH = 800;
+		static constexpr int HEIGHT = 600;
 
-private:
-	LveWindow lveWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
-};
+		void run();
+
+	private:
+		LveWindow lveWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
+		LvePipeline lvePipeline{ "src/shaders/simple_shader.vert.spv", "src/shaders/simple_shader.frag.spv" };
+	};
 }
